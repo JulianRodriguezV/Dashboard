ddf4f739b9aaffc8dbae44c3481e8d48e94935c6
JulianRodriguezV <juliancho7227@gmail.com>
2023-08-04 22:50:24 -0500

    prueba

--- src/App.js
+++ src/App.js
@@ -31,13 +31,13 @@ function App() {
       En per_page cambiamos el valor 100 por 4, ya que solo necesitamos las 4 primeras monedas, este valor puede ser cambiado de acuerdo a la información que quieran mostrar.
     */
     const response = await fetch(
-      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${selCur}&order=market_cap_desc&per_page=4&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d%2C90d%2C1y`
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${selCur}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d%2C90d%2C1y`
     );
 
     /* 
       Cuando hacemos el llamado de la API el valor que nos retorna es un arreglo de objetos tipo json, este valor lo vamos a almacenar en una variable y lo vamos a leer por medio del método json()
     */
-    const json = await response.json();
+    const json = response.ok ? await response.json() : null;
     
     /*
       La siguiente URL la generamos de la API para que nos permita acceder a diferentes divisas, esta URL la almacenamos en una variable.
